f0baea314dd2a0e64e610739c361765e94d3c0cd
Shreya Sapale <Shreyasapale@users.noreply.github.com>
2021-04-01 13:26:47 +0530

    New FlyodWrsh working

--- AOA/FlyodWarshall.c
+++ AOA/FlyodWarshall.c
@@ -1,87 +1,10 @@
 #include <stdio.h>
-#include <conio.h>
+#include <stdlib.h>
 #include <math.h>
-#define INT_MAX 9999
+#define INTEGER_MAX 9999
 int Adjacency_Matrix[10][10];
 int a[10][10];
 int Parent_matrix[10][10];
-
-// int extractmin(struct node nodearray[])
-// {
-//     int min = 0;
-//     int flag = 0;
-//     for (int i = 1; i <= num_nodes; i++)
-//     {
-//         if (nodearray[i].parent != -1)
-//         {
-//             flag = 1;
-//         }
-//     }
-//     if (flag == 0)
-//     {
-//         return 1;
-//     }
-//     else
-//     {
-//         for (int i = 2; i <= num_nodes; i++)
-//         {
-//             if (nodearray[i].parent != -1 && nodearray[i].status == 0)
-//             {
-//                 if (min == 0)
-//                     min = i;
-//                 else if (nodearray[min].distance > nodearray[i].distance)
-//                 {
-//                     min = i;
-//                 }
-//             }
-//         }
-//         return min;
-//     }
-// }
-// void dijshortest(struct node nodearray[])
-// {
-
-//     nodearray[1].distance = 0;
-//     for (int i = 1; i <= num_nodes; i++)
-//     {
-//         int u = extractmin(nodearray);
-//         printf("\n");
-//         printf("\n\nNODE %d : ", u);
-//         solution[i] = u;
-//         for (int j = 1; j <= num_nodes; j++)
-//         {
-//             if (a[u][j] != 0)
-//             {
-//                 printf("\n");
-//                 printf("\nPath %d to %d ", u, j);
-//                 if (nodearray[j].distance > (nodearray[u].distance + a[u][j]))
-//                 {
-//                     printf("\nFor %d : ", j);
-//                     printf("\nInitial distance : %d \n ", nodearray[j].distance);
-//                     nodearray[j].distance = nodearray[u].distance + a[u][j];
-//                     nodearray[j].parent = u;
-//                     printf("New Distance = %d \nParent = %d", nodearray[j].distance, u);
-//                 }
-//                 else
-//                 {
-//                     printf("\nThere is no change :\nDistance : %d \nParent is %d", nodearray[j].distance, nodearray[j].parent);
-//                 }
-//             }
-//         }
-//         nodearray[u].status = 1;
-//     }
-//     printf("\n\nFinal Shortest Distance for : ");
-//     for (int i = 1; i <= num_nodes; i++)
-//     {
-//         printf("\n %d  : %d", i, nodearray[i].distance);
-//     }
-//     printf("\n");
-//     printf("\nQueue S : ");
-//     for (int i = 1; i <= num_nodes; i++)
-//     {
-//         printf(" %d ", solution[i]);
-//     }
-// }
 
 void printtable(int num_nodes)
 {
@@ -131,33 +54,103 @@ void printtable(int num_nodes)
     }
 }
 
-int mindist(int initial, int new1, int new2)
+void printparent(int num_nodes)
 {
-    int new_distance;
-    if (new1 == INT_MAX || new2 == INT_MAX)
+    char parent;
+    // printf("\n _______________________________________________________________________________");
+    // printf("\n|               |               |               |               |               |");
+    // printf("\n|   Time Slot   |     Job No.   |    Profit     |   Deadline    |  Total Profit |");
+    // printf("\n|_______________|_______________|_______________|_______________|_______________|\t");
+    // printf("\n|               |               |               |               |               |");
+    printf("\n");
+    for (int i = 0; i <= num_nodes; i++)
     {
-        return initial;
+        printf(" ____");
     }
-    new_distance = new1 + new2;
-    if (new_distance < initial)
+    // printf("\n|");
+    // for (int i = 0; i <= num_nodes; i++)
+    // {
+    //     printf("    |");
+    // }
+    printf("\n|    |");
+    for (int i = 0; i < num_nodes; i++)
     {
-        return new_distance;
+        printf(" %c  |", i + 65);
     }
-    else
+    printf("\n|");
+    for (int i = 0; i <= num_nodes; i++)
     {
-        return initial;
+        printf("____|");
     }
+    printf("\n|");
+    for (int i = 0; i <= num_nodes; i++)
+    {
+        printf("    |");
+    }
+
+    for (int i = 1; i <= num_nodes; i++)
+    {
+        printf("\n| %c  |", i + 64);
+        for (int j = 1; j <= num_nodes; j++)
+        {
+            parent = Parent_matrix[i][j];
+            if (parent == -1)
+            {
+                parent = '-';
+            }
+            else
+            {
+                parent = parent + 64;
+            }
+            printf(" %c  |", parent);
+        }
+    }
+    printf("\n|");
+    for (int i = 0; i <= num_nodes; i++)
+    {
+        printf("____|");
+    }
+    printf("\n");
 }
+
+void displaypath(int source, int destination)
+{
+    int initialdestination = destination;
+    int i = 1;
+    // int sourceindex = source - 64;
+    // int destinationindex = destination - 64;
+    int sourceindex = source;
+    int destinationindex = destination;
+    int path[10];
+    path[1] = destinationindex;
+
+    while (Parent_matrix[sourceindex][destinationindex] != sourceindex)
+    {
+        i++;
+        path[i] = Parent_matrix[sourceindex][destinationindex];
+        destinationindex = Parent_matrix[sourceindex][destinationindex];
+    }
+    path[++i] = sourceindex;
+    printf("\nThe Path from %c to %c is : ", source + 64, destination + 64);
+    for (int j = i; j > 1; j--)
+    {
+        printf(" %c ->", path[j] + 64);
+    }
+    printf(" %c", destination + 64);
+
+    printf("\nThe shortest distance for %c to %c : %d", source + 64, destination + 64, a[sourceindex][destination]);
+}
+
 int mindistbool(int i, int j, int k)
 {
     // a[i][j] = mindist(a[i][j], a[i][k], a[k][j]);
 
     int new_distance;
-    if (a[i][k] == INT_MAX || a[k][j] == INT_MAX)
+    if (Adjacency_Matrix[i][k] == INTEGER_MAX || Adjacency_Matrix[k][j] == INTEGER_MAX)
     {
         return 0;
     }
-    if (a[i][k] + a[k][j] < a[i][j])
+    if (Adjacency_Matrix[i][k] + Adjacency_Matrix[k][j] < a[i][j])
     {
         return 1;
     }
@@ -167,24 +160,28 @@ int mindistbool(int i, int j, int k)
     }
 }
 
-void floydWrshl(int num_nodes)
+void changeMatrix(int k, int num_nodes)
 {
-    for (int i = 1; i < num_nodes; i++)
+
+    for (int i = 1; i <= num_nodes; i++)
     {
-        for (int j = 1; j < num_nodes; j++)
+        for (int j = 1; j <= num_nodes; j++)
         {
-            if (i == j)
-            {
-                a[i][j] = 0;
-            }
+            Adjacency_Matrix[i][j] = a[i][j];
         }
         // printtable(num_nodes);
     }
+}
+
+void floydWrshl(int num_nodes)
+{
     printf("\nInitial Matrix");
     printtable(num_nodes);
 
     for (int k = 1; k <= num_nodes; k++)
     {
+        changeMatrix(k, num_nodes);
+        printf("\n For Node  %c ", k + 64);
         for (int i = 1; i <= num_nodes; i++)
         {
             if (i == k)
@@ -207,10 +204,11 @@ void floydWrshl(int num_nodes)
                     continue;
                 }
                 // a[i][j] = mindist(a[i][j], a[i][k], a[k][j]);
-                printf("\n\na[%d][%d] = %d \na[%d][%d] = %d + %d = %d", i, j, a[i][j], i, j, a[i][k], a[k][j], a[i][k] + a[k][j]);
+                printf("\n\na[%d][%d] init = %d \na[%d][%d] = %d + %d ", i, j, a[i][j], i, j, Adjacency_Matrix[i][k], Adjacency_Matrix[k][j]);
                 if (mindistbool(i, j, k))
                 {
-                    a[i][j] = a[i][k] + a[k][j];
+                    a[i][j] = Adjacency_Matrix[i][k] + Adjacency_Matrix[k][j];
+                    Parent_matrix[i][j] = Parent_matrix[k][j];
                 }
                 printf("\n a[%d][%d] = %d", i, j, a[i][j]);
             }
@@ -221,8 +219,9 @@ void floydWrshl(int num_nodes)
 int main()
 {
     int num_nodes;
+    char destination, source;
     printf("\nAll pair shortest path: Floyd Warshall Algorithm\n");
-    printf("\nEnter the number of nodes : ");
+    printf("\nEnter the number of Vertices : ");
     scanf("%d", &num_nodes);
 
     //todo inputs
@@ -235,11 +234,32 @@ int main()
             scanf("%d", &edge);
             a[i][j] = edge;
             Adjacency_Matrix[i][j] = edge;
+            if (edge == INTEGER_MAX || edge == 0)
+            {
+                Parent_matrix[i][j] = -1;
+            }
+            else
+            {
+                Parent_matrix[i][j] = i;
+            }
         }
     }
     floydWrshl(num_nodes);
-    printf("\n\n\nFinal Shortest paths for all pairs : ");
+    printf("\n\n\nFinal Shortest distance for all pairs : ");
     printtable(num_nodes);
+
+    printf("\n\n\nFinal Parent Matrix is : ");
+    printparent(num_nodes);
+
+    printf("\nEnter the vertices to see the path : ");
+    printf("\nEnter Start Index : ");
+    scanf("%d", &source);
+
+    // scanf("%c %c", &source, &destination);
+    printf("Enter Destination Index : ");
+    scanf("%d", &destination);
+
+    displaypath(source, destination);
 
     return 0;
 }
